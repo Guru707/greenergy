03b18ad14b12750451f7762b1768dd238ae31230
Guru707 <gsraj77@gmail.com>
2020-09-04 14:31:45 +0530

    (design:implement cart page)

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,16 @@
 
+window.onscroll = function() {stickyFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function stickyFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 // $(".product-dtl-arrow a[href^='#']").click(function (e) {
 //   e.preventDefault();
 //   var position = $($(this).attr("href")).offset().top;
@@ -310,15 +322,3 @@ $(document).ready(function(){
 
 });
 
-window.onscroll = function() {stickyFunction()};
-
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function stickyFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
